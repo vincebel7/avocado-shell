f4fe0fbf5ca6d5cdef1a0cdc2b37b4f298157a7e
vincebelanger <vincebelanger25@gmail.com>
2017-12-13 18:22:28 -0500

    Attempt to add interface.c again

---
+++ interface.c
@@ -0,0 +1,90 @@
+/********************************************************************
+  
+USAGE AGREEMENT: The Avocado Shell (avo) is a free and open-source 
+shell written by Vince Belanger. Do whatever you want with it because
+I'm not a prick.
+ 
+VERSION: 0.0.2
+
+********************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<wait.h>
+
+#define SPACE " "
+#define SPACE_N " \n"
+#define TAB " \t"
+#define TAB_N " \t\n"
+
+void shell_line(){
+   //char usr[1024];
+   char cwd[1024];
+   printf("[avo: %s]> ", getcwd(cwd, sizeof(cwd))); 
+   //TODO: tokenize by forward slashes
+}
+
+int main(){
+   char* input = NULL;
+   size_t n = 100;
+   char** string = malloc(sizeof(*string) * n);
+   pid_t child = 0;
+   char** argv = NULL;
+   int argc;
+   char* file = malloc(sizeof(*file) * n);
+   int i;
+   char** newargv = malloc(sizeof(*newargv) * n);
+
+   shell_line();
+
+   while(getline(&input, &n, stdin) != EOF){
+      argv = malloc(sizeof(*argv) * n);
+
+      if(strcmp(input, TAB)) //tab delimited in input
+         *string = strtok(input, TAB_N);
+
+      else if(strcmp(input, SPACE_N)) //space delimited in input
+         *string = strtok(input, SPACE_N);
+
+      argc = 0;
+      while(*string){
+         argv[argc] = *string;
+         if(!argc) file = argv[0];
+
+         if(strcmp(input, TAB))
+            *string = strtok('\0', TAB_N);
+
+         else if(strcmp(input, SPACE_N))
+            *string = strtok('\0', SPACE_N);
+        
+         argc++;
+      }
+
+      if((child = fork()) == -1){
+         fprintf(stderr, "Child failed to fork\n");
+         exit(1);
+      }
+
+      for(i = 0; i < argc; i++)
+         newargv[i] = argv[i];
+
+      if(child == 0){
+         execvp(file, &newargv[0]);
+         fprintf(stderr, "./avosh: %s: No such file or directory\n", file);
+         exit(1);
+      }
+
+      else wait(NULL); 
+      free(argv);
+      
+      //ensures blank spaces aren't read as args for next command
+      for(i = 0; i < argc; i++)
+         newargv[i] = '\0';
+
+      shell_line();
+   }
+   exit(0);
+}
+
